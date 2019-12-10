773cf00406bc7b5ed5aef318f3ebff935a329190
Joseph Saah <jsaah@gmu.edu>
2019-12-09 23:03:27 -0500

    Add health points to bullet and reflect off player while maintaining constant speed

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -1,110 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public BulletParticleManager particles;
     public float moveSpeed = 5;
-
     public Vector2 moveDir = Vector2.one / 2;
+
+    protected int health; // TODO: Set to 1 in BoidBullet
     protected Rigidbody2D rb;
+    protected float rbMagnitude;
+
     private SpriteRenderer sr;
 
-    public BulletParticleManager particles;
+    public int Health
+    {
+        get { return health; }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        health = Random.Range(1, 4); // random health points between 1 and 3
+        Debug.Log("Health: " + health);
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
-        transform.rotation = Quaternion.Euler(0, 0, 45);
         rb.AddForce(moveDir * moveSpeed);
+        rbMagnitude = rb.velocity.magnitude;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // get magnitude after collision
+        rbMagnitude = rb.velocity.magnitude;
     }
 
     private void FixedUpdate()
     {
-        //rb.MovePosition((Vector2)transform.position + (moveDir * moveSpeed * Time.deltaTime));
-    }
 
-
-    public void UpdateAngle(float newAngle)
-    {
-        Quaternion curRotation = transform.rotation;
-        Vector3 eulerAngles = curRotation.eulerAngles;
-        eulerAngles.z = newAngle;
-        curRotation.eulerAngles = eulerAngles;
-        transform.rotation = curRotation;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {        
-        // handle environment collisions
-        if (collision.gameObject.layer == 8)
-        {
-            float vX = rb.velocity.x;
-            float vY = rb.velocity.y;
-            //Debug.Log(vY);
-            Vector3 eulerAngles = transform.rotation.eulerAngles;
-            Vector2 velocity = rb.velocity;
-            if (vX > 0)
-            {
-                if (vY < 0)
-                {
-                    //CalculateCollisionAngle(collision.GetContact(0).point);
-                    float temp = moveDir.y;                    
-                    temp++;
-                    moveDir.y = temp;
-                    UpdateAngle(eulerAngles.z + 90);
-                }                    
-                else
-                {
-                    //CalculateCollisionAngle(collision.GetContact(0).point);
-                    moveDir.y -= 1f;
-                    UpdateAngle(eulerAngles.z - 90);
-                }
-                    
-            }
-            else
-            {
-                if (vY > 0)
-                    UpdateAngle(eulerAngles.z + 90);
-                else
-                    UpdateAngle(eulerAngles.z - 90);
-            }
-
-            // --SUBSYSTEM --
-            particles.EnableBurst();
-        }
-        else if(collision.gameObject.layer == 9)
-        {
-            particles.EnableDeath();
-        }
-    }
-    
-    void CalculateCollisionAngle(Vector2 collisionPoint)
     {
-        // layermask = 8 because ( 8 ) is the layer for environment
-        // layermask =  ~layermask inverts layermask and now ignores all other layers
-        int ignoreLayers = 8;
-        ignoreLayers = ~ignoreLayers;
+        // check collision with player
+        if (collision.gameObject.layer == 9)
+        {
+            // decrement health and destroy bullet if health is 0
+            if (--health == 0)
+            {
+                particles.EnableDeath();
+            }
+        }
 
-        RaycastHit2D hit;
-        hit = Physics2D.Raycast(transform.position, rb.velocity, Mathf.Infinity, ignoreLayers);
+        // maintain current magnitude after collision
+        rb.velocity = rb.velocity.normalized * rbMagnitude;
 
-        Debug.Log("Collision point " + collisionPoint);
-        Debug.Log("Raycast hit " + hit.point);
-
-        float colX = hit.point.x - collisionPoint.x;
-        float colY = hit.point.y - collisionPoint.y;
-        float mag = Mathf.Sqrt((colX * colX + colY * colY));
-        float angle = Mathf.Acos(colY / mag) * 180 / Mathf.PI;
-
-        Debug.Log(angle);
-    }    
+        // --SUBSYSTEM --
+        particles.EnableBurst();
+    }
 }
