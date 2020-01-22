fca36903cd0fc8edf52a16ca19fdedae8582d9aa
awaters9@masonlive.gmu.edu <awaters9@masonlive.gmu.edu>
2020-01-21 23:41:33 -0500

    Added initial test for new bomb projectile

---
+++ Assets/Scripts/Projectiles/Bomb/Bomb.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject explosionCol;
+
+    private Rigidbody2D rb;
+    private CircleCollider2D col;
+
+    public float fuseTime = 0.5f;
+    private float fuseTimer;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CircleCollider2D>();
+        StartCoroutine(SetFuse());
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        
+    }
+
+    //Bombs have a ticking mechanic
+    IEnumerator SetFuse()
+    {
+        yield return new WaitForSeconds(fuseTime);
+        DetonateBomb();
+    }
+
+    //Bombs explode
+    void DetonateBomb()
+    {
+        Debug.Log("Detonate bomb");
+        explosionCol.SetActive(true);
+    }
+
+    //Bombs can also destroy bullets if deflected
+
+}
---
+++ Assets/Scripts/Projectiles/Bomb/Bomb_ExplosionCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb_ExplosionCollider : MonoBehaviour
+{
+    private CircleCollider2D col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<CircleCollider2D>();
+        gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(DecreaseHitBoxSize());
+    }
+
+    IEnumerator DecreaseHitBoxSize()
+    {
+        col.radius = 2.45f;
+        while(col.radius >= 0.5f)
+        {
+            col.radius -= 0.05f;
+            yield return null;
+        }
+        gameObject.SetActive(false);
+    }
+}
